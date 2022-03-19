42fed0a36196bd6e17b7356fd7472bfd4aa57239
adria <littleivanitsky@gmail.com>
2022-03-18 17:42:44 -0700

    first test suite complete

--- __tests__/rocks.test.js
+++ __tests__/rocks.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Rock = require('../lib/models/Rock');
 
 describe('hand-of-resources routes', () => {
   beforeEach(() => {
