fd8e35ac1271e2538c322d59768d766b167a74a3
adria <littleivanitsky@gmail.com>
2022-03-18 18:27:12 -0700

    test 1 of 5 passing

--- __tests__/guitars.test.js
+++ __tests__/guitars.test.js
@@ -5,19 +5,20 @@ const app = require('../lib/app');
 const Guitar = require('../lib/models/Guitar');
 
 describe('guitar routes', () => {
-    beforeEach(() => {
-      return setup(pool);
-    });
-  
-    afterAll(() => {
-      pool.end();
-    });
+  beforeEach(() => {
+    return setup(pool);
+  });
 
-    test('should create a table row', async () => {
-        const expected = {
-            name: 'gibson les paul', 
-            year: '1982',
-        };
-        const response = await (await request(app).post('/api/v1/guitars')).setEncoding(expected);
-        expect(response.body).toEqual({ id: expect.any(String), ...expected });
-    });
+  afterAll(() => {
+    pool.end();
+  });
+
+  test('should create a table row', async () => {
+    const expected = {
+      name: 'gibson les paul',
+      year: '1982',
+    };
+    const response = await request(app).post('/api/v1/guitars').send(expected);
+    expect(response.body).toEqual({ id: expect.any(String), ...expected });
+  });
+});
