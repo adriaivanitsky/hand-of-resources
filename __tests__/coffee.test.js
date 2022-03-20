6ca91b3dfa0540bd615ab64497e8eba402c58e6c
adria <littleivanitsky@gmail.com>
2022-03-19 21:35:18 -0700

    test 2 of 5 written

--- __tests__/coffee.test.js
+++ __tests__/coffee.test.js
@@ -4,7 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const Coffee = require('../lib/models/Coffee');
 
-describe('journal routes', () => {
+describe('coffee routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
@@ -20,5 +20,23 @@ describe('journal routes', () => {
     };
     const response = await request(app).post('/api/v1/coffees').send(expected);
     expect(response.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  test('should read the table', async () => {
+    const expected = [
+      { id: '1', name: 'stumptown', roast: 'light' },
+      {
+        id: '2',
+        name: 'coava',
+        roast: 'medium',
+      },
+      {
+        id: '3',
+        name: 'portland coffee roasters',
+        roast: 'dark',
+      },
+    ];
+    const response = await request(app).get('/api/v1/coffees');
+    expect(response.body).toEqual(expected);
   });
 });
